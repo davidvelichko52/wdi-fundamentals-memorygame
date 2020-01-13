ce7bcd03cefe472f755c4fcb7dde0124f5dbb161
davidvelichko52 <55528860+davidvelichko52@users.noreply.github.com>
2020-01-13 15:44:25 -0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -35,25 +35,33 @@ function checkForMatch(){
 }
 
 
-}
+};
 
-function flipCard(cardId){
+function flipCard(){
+	let cardId = this.getAttribute('data-id');
 
 	cardsInPlay.push(cards[cardId].rank);
 
-    console.log("User flipped " +cards[cardId].rank);
-    console.log(cards[cardId].cardImage);
-    console.log(cards[cardId].suit);
-if (cardsInPlay.length == 2){
+    this.setAttribute('src', cards[cardId].cardImage);
+if (cardsInPlay.length === 2){
 
 checkForMatch();
 }
 
-        
+}
 
-      }
+      console.log(cardsInPlay);
 
-      flipCard(0);
-      flipCard(2);
   
+  function createBoard(){
+     const cardTable = document.getElementById('game-board');
+  	for (let i = 0; i < cards.length; i++) {
+  	let cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    cardTable.appendChild(cardElement);
+  	}
+  }
 
+createBoard();
